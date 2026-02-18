82c9435f8e1dad953cb405272c5ecfaceb96645a
Pongchanan <pongchanansriwanna2547@gmail.com>
2026-02-18 09:54:32 +0700

    fix deploy 2

--- backend/main.py
+++ backend/main.py
@@ -96,4 +96,5 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
