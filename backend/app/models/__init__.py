1e017839a7e536f8d66f8d89c3229d478ae6a7cc
plscallMeAlex <alexkung007@gmail.com>
2026-04-05 14:01:58 +0700

    model for damaged items

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.user import User
 from app.models.borrowing import Borrowing
 from app.models.item import Item
 from app.models.open_session import OpenSession
+from app.models.damaged_item_report import DamagedItemReport
 
-__all__ = ["User", "Borrowing", "Item", "OpenSession"]
+__all__ = ["User", "Borrowing", "Item", "OpenSession", "DamagedItemReport"]
